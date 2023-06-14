a306c3ae9e377fa40e893f3e6d051676a4c14751
Jorge <jorgelarteagap@gmail.com>
2023-06-14 16:26:42 -0400

    Title rounds creates

--- src/app/components/pairs/Braket.js
+++ src/app/components/pairs/Braket.js
@@ -1,26 +1,31 @@
 'use client'
 import React, { useState } from 'react'
 
-export const Bracket = ({ player1, player2 }) => {
-  const [selectedParticipant, setSelectedParticipant] = useState(null)
+export const Bracket = ({ player1, player2, onWinnerSelected }) => {
+  const [winner, setWinner] = useState(null)
 
-  const handleClickParticipant = participant => {
-    setSelectedParticipant(participant)
+  const handleWinnerSelect = selectedPlayer => {
+    setWinner(selectedPlayer)
+    onWinnerSelected(selectedPlayer)
   }
 
   return (
     <div className="is-flex is-flex-direction-column" id="bracket">
       <div
-        className="button is-success is-outlined my-2"
-        onClick={() => handleClickParticipant(1)}
-        disabled={selectedParticipant === 2}
+        className={`button is-success is-outlined my-2 ${
+          winner === player1 ? 'is-active' : ''
+        }`}
+        onClick={() => handleWinnerSelect(player1)}
+        disabled={winner !== null}
       >
         {player1}
       </div>
       <div
-        className="button is-danger is-outlined my-2"
-        onClick={() => handleClickParticipant(2)}
-        disabled={selectedParticipant === 1}
+        className={`button is-danger is-outlined my-2 ${
+          winner === player2 ? 'is-active' : ''
+        }`}
+        onClick={() => handleWinnerSelect(player2)}
+        disabled={winner !== null}
       >
         {player2}
       </div>
--- src/app/components/pairs/OrderPairs.js
+++ src/app/components/pairs/OrderPairs.js
@@ -1,5 +1,7 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { Bracket } from './Braket'
+
 const players = [
   'Participant 1',
   'Participant 2',
@@ -8,49 +10,75 @@ const players = [
   'Participant 5',
   'Participant 6',
   'Participant 7',
-  'Participant 8'
+  'Participant 8',
+  'Participant 9',
+  'Participant 10',
+  'Participant 11',
+  'Participant 12',
+  'Participant 13',
+  'Participant 14',
+  'Participant 15',
+  'Participant 16'
 ]
 
 export const OrderBrackets = () => {
-  const brackets = []
-  for (let i = 0; i < players.length; i += 2) {
-    const player1 = players[i]
-    const player2 = players[i + 1]
-    brackets.push({ player1, player2 })
+  const [winners, setWinners] = useState([...players])
+
+  const handleWinnerSelected = winner => {
+    setWinners(prevWinners => [...prevWinners, winner])
   }
 
-  return (
-    <>
-      {brackets.map((bracket, index) => (
-        <Bracket
-          key={index}
-          player1={bracket.player1}
-          player2={bracket.player2}
-        />
-      ))}
-    </>
-  )
+  const renderBracketRounds = players => {
+    const numParticipants = players.length
+    const bracketRounds = []
+    let currentRoundPlayers = players
+
+    while (currentRoundPlayers.length > 1) {
+      const roundBrackets = []
+      const halfLength = Math.ceil(currentRoundPlayers.length / 2)
+
+      for (let i = 0; i < halfLength; i++) {
+        const player1 = currentRoundPlayers[i * 2] || ''
+        const player2 = currentRoundPlayers[i * 2 + 1] || ''
+
+        roundBrackets.push(
+          <Bracket
+            key={i}
+            player1={player1}
+            player2={player2}
+            onWinnerSelected={handleWinnerSelected}
+          />
+        )
+      }
+
+      bracketRounds.push(roundBrackets)
+      currentRoundPlayers = roundBrackets.map(bracket => bracket.props.winner)
+    }
+
+    return bracketRounds
+  }
+
+  const renderBrackets = () => {
+    const bracketRounds = renderBracketRounds(winners)
+    const numRounds = Math.ceil(Math.log2(players.length))
+
+    return bracketRounds.map((round, index) => (
+      <div key={index} className="bracket-round">
+        <h2>{getTitleByRound(index + 1, numRounds)}</h2>
+        {round}
+      </div>
+    ))
+  }
+
+  const getTitleByRound = (round, numRounds) => {
+    if (round === numRounds) {
+      return 'Final'
+    } else if (round === numRounds - 1) {
+      return 'Semifinal'
+    } else {
+      return `Round ${round}`
+    }
+  }
+
+  return <div className="bracket-container">{renderBrackets()}</div>
 }
-
-// export const OrderPairs = () => {
-//   return (
-//     <>
-//       {participants.map((participant, i) => {
-//         return (
-//           <div
-
-//             key={i}
-//             style={{
-//               borderColor: 'white',
-//               padding: '1rem',
-//               borderStyle: 'solid',
-//               borderBlockWidth: '1px'
-//             }}
-//           >
-//             {participant}
-//           </div>
-//         )
-//       })}
-//     </>
-//   )
-// }
